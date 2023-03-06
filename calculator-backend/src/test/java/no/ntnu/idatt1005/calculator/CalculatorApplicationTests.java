7213f8025380a1ffd36366304ccec0e975a3495c
Edvard Sørby <117030279+edvardsorby@users.noreply.github.com>
2023-03-06 11:21:17 +0100

    Initial commit

---
+++ calculator-backend/src/test/java/no/ntnu/idatt1005/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.idatt1005.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
